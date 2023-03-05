d8091710a63be5b75fb784a618190d790392f659
mamin98 <mohamedamin1421@gmail.com>
2023-03-05 04:29:00 +0200

    declare role admin and add viewmodel

---
+++ Restaurant/Restaurant/Controllers/RoleController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.ViewModels.Role;
+
+namespace Restaurant.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        // DI
+        public RoleController(RoleManager<IdentityRole> RoleManager)
+        {
+            roleManager = RoleManager;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RoleViewModel roleVM)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = roleVM.RoleName;
+                IdentityResult result = await roleManager.CreateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return View();
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+      
+            return View(roleVM);
+        }
+    }
+}
---
+++ Restaurant/Restaurant/ViewModels/Role/RoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.ViewModels.Role
+{
+    public class RoleViewModel
+    {
+        [MinLength(3)]
+        [DisplayName("Role Value")]
+        [Required]
+        public string RoleName { get; set; }
+
+    }
+}
