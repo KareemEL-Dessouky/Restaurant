5076e75e7013d06044c84c39f6b1fcf1319f4322
Amr Abdelkamel <amromoorie@gmail.com>
2023-03-06 08:56:31 +0200

    Developed Add to card ctrl

--- Restaurant/Restaurant/Controllers/CartController.cs
+++ Restaurant/Restaurant/Controllers/CartController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
 
 using Restaurant.Models;
 using Restaurant.Repositories;
@@ -8,85 +12,66 @@ namespace Restaurant.Controllers
 {
     public class CartController : Controller
     {
-        private readonly IGenericRepository<Product> _productRepository;
-        private readonly IGenericRepository<OrderItems> _orderItemsRepository;
-        private readonly IGenericRepository<Customer> _customerRepository;
-        private readonly IGenericRepository<Order> _orderRepository;
-        public CartController(IGenericRepository<Product> productRepository, IGenericRepository<OrderItems> orderItemsRepository, IGenericRepository<Customer> customerRepository, IGenericRepository<Order> orderRepository)
+        private readonly IGenericRepository<Product> _product;
+        private readonly IGenericRepository<OrderItems> _orderItems;
+        private readonly IGenericRepository<Customer> _customer;
+        private readonly IGenericRepository<Order> _order;
+        private readonly RestaurantContext _context;
+        private readonly IMapper _mapper;
+        public CartController(IGenericRepository<Product> productRepository, IGenericRepository<OrderItems> orderItemsRepository, IGenericRepository<Customer> customerRepository, IGenericRepository<Order> orderRepository, IMapper mapper, RestaurantContext context)
         {
-            _productRepository = productRepository;
-            _orderItemsRepository = orderItemsRepository;
-            _customerRepository = customerRepository;
-            _orderRepository = orderRepository;
+            _product = productRepository;
+            _orderItems = orderItemsRepository;
+            _customer = customerRepository;
+            _order = orderRepository;
+            _mapper = mapper;
+            _context = context;
         }
 
-        /// <summary>
-        /// NEED repository service for testing, based on the following actions
-        /// Actions: add, remove, view checkout info, perform checkout and redirect to thank you page
-        /// Final Order receipt action based on CustomerOrderRepository
-        /// </summary>
 
-        // GET: /Cart
+
+        // GET: /Cart/AddtoCart
         // get related cart info for specific customer
-        public IActionResult Index(CartCheckOutViewModel vm)
+        public IActionResult AddToCart(int id, int quantity)
         {
-            // get all orders by customer id
-            //var orderedByCustomer = _orderRepository.GetAllOrdersByCustomerId(id);
+            var addedProduct = _product.GetByID(id);
 
-
-            //var cartItems = _orderItemsRepository.GetByID(id);
-
-            // fill the CartCheckOutViewModel from tables:
-            // CustomerOrders, Products, Customer, OrderItems
-
-
-            return Content("test");
-        }
-
-
-
-        public IActionResult AddToCart(int id)
-        {
-            var product = _productRepository.GetByID(id);
-            if (product != null)
+            if (addedProduct == null)
             {
-                // add the product to card
-                // decrement quantity of one (from order items table)
+                // output msg: try again
+                return RedirectToAction("Index");
             }
-            return RedirectToAction("Index");
+
+
+            var mappedProduct = _mapper.Map<CartItemViewModel>(addedProduct);
+            mappedProduct.Quantity = quantity;
+
+            // save cart item to session
+            HttpContext.Session.SetString("CartItem", JsonConvert.SerializeObject(mappedProduct));
+
+
+            // Redirect user to checkout page
+            return RedirectToAction("Checkout");
+
         }
 
-        public IActionResult RemoveFromCart(int id)
+        // GET: /Cart/Checkout
+        [HttpGet]
+        public IActionResult Checkout(CartItemViewModel cartItemViewModel)
         {
-            var product = _productRepository.GetByID(id);
-            if (product != null)
-            {
-                // remove the selected product
-                // decrement quantity by 1 (from order items table)
-            }
-            return RedirectToAction("Index");
+            // get and deserialize data json
+            var cartItemJson = HttpContext.Session.GetString("CartItem");
+            var cartItem = cartItemJson != null ?
+                JsonConvert.DeserializeObject<CartItemViewModel>(cartItemJson) : null;
+
+            var order = _context.OrderItems.FirstOrDefault(o => o.ProductID == cartItem.Id);
+
+            // decrease the items quantity according to user choice
+            order.Quantity -= cartItem.Quantity;
+
+            var mappedProduct = _mapper.Map<CartItemViewModel>(cartItem);
+            return View(mappedProduct);
         }
-
-
-        [HttpPost]
-        public IActionResult Checkout(CartCheckOutViewModel vm)
-        {
-            if (ModelState.IsValid)
-            {
-                // Get Checkout Cart Info
-
-                // Create the Order
-
-                // redirect to thank you page if successful
-
-
-
-                return RedirectToAction("Complete");
-            }
-            return View(vm);
-        }
-
-
     }
 }
 
