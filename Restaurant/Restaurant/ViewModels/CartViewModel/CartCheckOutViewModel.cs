8ebf7042d63b96c185a352d5c5df5d24e392b925
Amr Abdelkamel <amromoorie@gmail.com>
2023-03-06 09:00:49 +0200

    Update views with add to cart form

--- Restaurant/Restaurant/ViewModels/CartViewModel/CartCheckOutViewModel.cs
+++ Restaurant/Restaurant/ViewModels/CartViewModel/CartCheckOutViewModel.cs
@@ -3,10 +3,7 @@
 public class CartCheckOutViewModel
 {
     public string CustomerName { get; set; } = null!;
-    public decimal TotalPrice { get; set; }
     public string? OrderNotes { get; set; }
-    public string ProductItem { get; set; } = null!;
-    public int Quantity { get; set; }
-    public IEnumerable<CartItemViewModel> OrderItems { get; set; } = new List<CartItemViewModel>();
+    public CartItemViewModel CartItem { get; set; } = null!;
 }
 
